5f5d97246c8150c5011ac8fb039994fa48593616
ghaiethboos1 <baaouniaghaieth@gmail.com>
2022-06-20 16:27:17 +0100

    commit initial

---
+++ fin d'anne.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<style type="text/css">
+		.but1
+	</style>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css" integrity="sha512-8bHTC73gkZ7rZ7vpqUQThUDhqcNFyYi2xgDgPDHc+GXVGHXq+xPjynxIopALmOPqzo9JZj0k6OqqewdGO3EsrQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js" integrity="sha512-dqw6X88iGgZlTsONxZK9ePmJEFrmHwpuMrsUChjAw1mRUhUITE5QU9pkcSox+ynfLhL15Sv2al5A0LVyDCmtUw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</head>
+<body><div class="ui segment">
+	<a class="ui inverted blue segment" href="">LOGO</a>
+	<div class="ui header right floated">
+		<a class="ui button" href="">accueil</a>
+		<a class="ui button" href="">utlisteur</a>
+		<a class="ui button" href="">content</a>
+	</div>
+</div> 
+	<form class="ui form">
+	<div class="ui raised segment">
+	<div>
+	<label>id</label>
+	<input type="text" name="id">
+     </div>
+     <div>
+	<label>nom darticle</label>
+	<input type="text" name="nom darticle">
+     </div>
+     <div >
+	<label>prix</label>
+	<input type="number" name="prix">
+     </div>
+     <div>
+	<label>color</label>
+	<input type="color" name="color">
+	<div id="but1">
+	<input  type="submit" value="envoyer">
+ </div>
+ </div>
+     </div>
+	</form>
+	<<?php  ?>
+</body>
+</html>
